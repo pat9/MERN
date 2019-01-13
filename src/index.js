c36610f26df098c4c768a4a852551976fcd3bb84
Daniel Pat <danielpat6@gmail.com>
2019-01-13 13:46:19 -0600

    Instalacio de nodemon y configuracion de express

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 //Congiruacion
-
+app.set("port", process.env.PORT || 3000);
 //Middleware
+app.use(morgan('dev'));
+app.use(express.json());
+//Routes
 
-app.listen(3000,()=>{
-    console.log("Server is running in localhost:3000")
+//Static Files
+
+//Server
+app.listen(app.get("port"),()=>{
+    console.log(`Server is running in ${app.get("port")}`)
 });
