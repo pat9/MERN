d47ddd0c2e294a7c9282a6b34a29360c6623a5ac
Daniel Pat <danielpat6@gmail.com>
2019-01-13 13:59:36 -0600

    Configuracion de rutas y archivos estaticos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path')
 const app = express();
 
 //Congiruacion
@@ -8,9 +9,9 @@ app.set("port", process.env.PORT || 3000);
 app.use(morgan('dev'));
 app.use(express.json());
 //Routes
-
+app.use("/api/tasks",require("./routes/task.routes.js"));
 //Static Files
-
+app.use(express.static(path.join(__dirname, "public")))
 //Server
 app.listen(app.get("port"),()=>{
     console.log(`Server is running in ${app.get("port")}`)
