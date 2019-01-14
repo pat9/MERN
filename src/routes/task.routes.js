e9872bb827500759c7bcce0ed95e8a307557e394
Daniel Pat <danielpat6@gmail.com>
2019-01-13 21:02:26 -0600

    Listo el rest de tasks con todas las rutas

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
-const TaskSchema = require("../models/task")
+const Task = require("../models/task");
 
-router.get("/", (req, res) =>{
-    res.json({
-        status:"api works!"
-    })
+router.get("/", async (req, res) =>{
+    const tasks = await Task.find();
+    res.json(tasks);
 });
+
+router.get("/:id", async (req, res) => {
+    const task = await Task.findById(req.params.id);
+    res.json(task);
+})
+
+router.post("/", async(req, res) =>{
+    const {title, description} = req.body;
+    const task = new Task({title, description});
+    await task.save();
+    res.json({status:"Task saved"});
+});
+
+router.put("/:id", async(req, res) =>{
+    const { title, description } = req.body;
+    const task = ({title, description});
+    await Task.findOneAndUpdate(req.params.id,task);
+    res.json({status:"Task updated"});
+})
+
+router.delete("/:id", async(req,res)=>{
+    await Task.findOneAndDelete(req.params.id)
+    res.json({status:"Task delated"})
+})
 
 module.exports = router;
